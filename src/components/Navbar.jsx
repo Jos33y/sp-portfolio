1bb769beb63e3038e82a5827c0e8bcd8b99847b8
Jos33y <josephlagbalu@gmail.com>
2023-05-10 07:15:09 -0500

    today section done stopped at 1:23:23 seconds

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   return (
+    // http://Kolawoleolayinka.tech
     <nav
       className={`${styles.paddingX
         } w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -22,9 +23,9 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={logo} alt="logo" className="w-12 h-12 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Pamino &nbsp;<span className="sm:block hidden">| Olayinka Kolawole </span>
+            Pamino.js &nbsp;<span className="sm:block hidden">| Olayinka Kolawole </span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
---
+++ src/hoc/index.js
@@ -0,0 +1,2 @@
+import SectionWrapper from "./wrapper";
+ export { SectionWrapper}
